527d43c46544d6b8a526c3538e9ad559f6606577
Henry Will <99243374+henry-will@users.noreply.github.com>
2022-08-23 15:14:06 +0900

    Add an old and new combination on erc20 testing

--- test/erc20/run_testcase.sh
+++ test/erc20/run_testcase.sh
@@ -1,6 +1,6 @@
-#!/usr/bin/bash
+#!/usr/bin/env bash
 set -e # exit on first error
 source setting-001.source
-node erc20-deploy.js
-node erc20-transfer-1step.js
-node erc20-transfer-2step.js
+node ERC20-deploy.js
+node ERC20-transfer-1step.js
+node ERC20-transfer-2step.js
